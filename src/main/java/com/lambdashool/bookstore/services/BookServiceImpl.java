9317757b48689120d90a31919101bf046dcd63a2
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-06-21 14:26:05 -0400

    Add BookService and BookServiceImpl, findAll and delete

--- src/main/java/com/lambdashool/bookstore/services/BookServiceImpl.java
+++ src/main/java/com/lambdashool/bookstore/services/BookServiceImpl.java
@@ -1,4 +1,38 @@
 package com.lambdashool.bookstore.services;
 
-public class BookServiceImpl {
+import com.lambdashool.bookstore.models.Book;
+import com.lambdashool.bookstore.repos.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+
+@Service(value = "bookService")
+public class BookServiceImpl implements BookService
+{
+    @Autowired
+    private BookRepository bookrepos;
+
+    @Override
+    public ArrayList<Book> findAll()
+    {
+        ArrayList<Book> list = new ArrayList<>();
+        bookrepos.findAll().iterator().forEachRemaining(list::add);
+        return list;
+    }
+
+    @Transactional
+    @Override
+    public void delete(long id) throws EntityNotFoundException
+    {
+        if (bookrepos.findById(id).isPresent())
+        {
+            bookrepos.deleteById(id);
+        } else
+        {
+            throw new EntityNotFoundException(Long.toString(id));
+        }
+    }
 }
